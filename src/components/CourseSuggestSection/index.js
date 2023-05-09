c5df5a9d1cc55108099539602edbc2271c597161
Micky Jittjana <immicky.a@gmail.com>
2023-05-09 14:31:19 +0700

    As a user, I can see Specific Courses and Course Roadmaps that I might be interested in Class Basket (#7)
    * Update Component for course suggestion

---
+++ src/components/CourseSuggestSection/index.js
@@ -0,0 +1,80 @@
+/**
+ * Module Name: CourseSuggestSection
+ *
+ * Depedency Modules:
+ *  - CartAdapter
+ */
+class CourseSuggestSection {
+  constructor(elementRef) {
+    this.elementRef = elementRef;
+    this.selectors = {
+      courseRoadmapList: ".levels-program_wrapper",
+      courseList: ".course-suggest_wrapper",
+    };
+
+    this.courseRoadmapList = this.elementRef.querySelector(
+      this.selectors.courseRoadmapList
+    );
+    this.courseList = this.elementRef.querySelector(this.selectors.courseList);
+
+    this._addEventListeners();
+    this._refreshDisplay();
+    this._refreshCourseSuggestion();
+  }
+
+  // Private
+
+  _addEventListeners() {
+    window.addEventListener("cart-updated", () => {
+      this._refreshDisplay();
+      this._refreshCourseSuggestion();
+    });
+  }
+
+  _refreshDisplay() {
+    if (CartAdapter.listCartItems().length > 0) {
+      this.courseList.style.display = "block";
+      this.courseRoadmapList.style.display = "none";
+    } else {
+      this.courseList.style.display = "none";
+      this.courseRoadmapList.style.display = "block";
+    }
+  }
+
+  _refreshCourseSuggestion() {
+    window.fsAttributes = window.fsAttributes || [];
+    window.fsAttributes.push([
+      "cmsfilter",
+      (filterInstances) => {
+        const courseSuggestInstance = filterInstances.find(
+          (instance) => instance.form.dataset.name == "course-suggest"
+        );
+
+        const filter = new Set();
+        const cartItems = CartAdapter.listCartItems();
+
+        if (cartItems.length > 0) {
+          cartItems
+            .flatMap((item) => item.attr.levels)
+            .forEach((level) => {
+              filter.add(level);
+            });
+        } else {
+          filter.add("EMPTY_SUGGESTION");
+        }
+
+        courseSuggestInstance.filtersData = [
+          {
+            filterKeys: ["level-slug"],
+            originalFilterKeys: ["level-slug"],
+            hightlight: false,
+            elements: [],
+            values: filter,
+          },
+        ];
+
+        courseSuggestInstance.applyFilters();
+      },
+    ]);
+  }
+}
